cc52e045da62afb63c98f12c34bd0defe11ef0fa
lain.lai.jiang@gmail.com <lain.lai.jiang@gmail.com>
2015-09-10 00:55:47 -0500

    Implimented method to host index.html

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var handler = require("./request-handler");
 var initialize = require("./initialize.js");
+var request = require('request');
 
 // Why do you think we have this here?
 // HINT: It has to do with what's in .gitignore
@@ -10,6 +11,7 @@ var port = 8080;
 var ip = "127.0.0.1";
 var server = http.createServer(handler.handleRequest);
 
+
 if (module.parent) {
   module.exports = server;
 } else {
@@ -17,3 +19,18 @@ if (module.parent) {
   console.log("Listening on http://" + ip + ":" + port);
 }
 
+
+// var request = require('request');
+// request('http://www.google.com', function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(body) // Show the HTML for the Google homepage. 
+//   }
+// })
+
+// http.createServer(function (req, resp) {
+//   if (req.url === '/doodle.png') {
+//     var x = request('http://mysite.com/doodle.png')
+//     req.pipe(x)
+//     x.pipe(resp)
+//   }
+// })
